90254dbcab83a3daad27b730b92650ca9d4d92d1
Guillaume BERNARD <gbernard42@outlook.fr>
2016-10-04 18:59:01 +0200

    First commit

---
+++ VendingMachine/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Client
+    {
+        public int MakeChoice()
+        {
+
+
+            return 0;
+        }
+
+        public int PutMoney()
+        {
+
+
+            return 0;
+        }
+    }
+}
---
+++ VendingMachine/Machine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Machine
+    {
+        public bool GetMoney()
+        {
+
+
+            return false;
+        }
+
+        public bool ThrowProduct()
+        {
+
+            return false;
+        }
+    }
+}
